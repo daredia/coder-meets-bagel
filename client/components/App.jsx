973c27d2cf2532f35607368bc102c5eb94ffa50a
Shehzad Daredia <shehzad.daredia@gmail.com>
2017-04-18 22:21:09 -0700

    Add basic form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,11 +3,28 @@ import React from 'react';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('link was submitted:', this.state.value);
+    this.setState({value: ''});
+    event.preventDefault();
   }
 
   render() {
     return (
-      <div>Welcome to Coder Meets Bagel!</div>
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="link" value={this.state.value} onChange={this.handleChange} autoFocus/>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
